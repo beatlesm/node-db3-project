45cc1ab1a17559cbc3fc66f74861ac21d72b9811
beatlesm <rj.geng@gmail.com>
2022-02-16 13:34:42 -0800

    Task 2: add codes to index.js and api/server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express")
+const logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors')
 
 const SchemeRouter = require('./schemes/scheme-router.js');
 
-const server = express();
+const server = express()
+server.use(logger('dev'));
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors()) // cors() returns a (req, res, nex) => { // stuff and then next() }
+
 server.use('/api/schemes', SchemeRouter);
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+  })
 
 module.exports = server;
